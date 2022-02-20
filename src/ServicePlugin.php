0c4edadc7b5d7c3a5a6d83d010f04b65dc1d2b02
SirDomin <domin12wsx@gmail.com>
2022-02-20 15:30:28 +0100

    Initial push

--- src/ServicePlugin.php
+++ src/ServicePlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace SirDomin\ServicePlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class ServicePlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
